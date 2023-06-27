b6215bdcc1d879ab90f75af626b24eb97d9bb52d
johannpa <johann_paque@hotmail.fr>
2023-06-27 15:49:25 +0200

    Add documentation with swagger

--- src/auth/dto/signupDto.ts
+++ src/auth/dto/signupDto.ts
@@ -1,11 +1,17 @@
+import { ApiProperty } from "@nestjs/swagger"
 import { IsEmail, IsNotEmpty } from "class-validator"
 
 export class SignupDto {
+    @ApiProperty()
     @IsNotEmpty()
     readonly userName: string
+
+    @ApiProperty()
     @IsNotEmpty()
     @IsEmail()
     readonly email: string
+    
+    @ApiProperty()
     @IsNotEmpty()
     readonly password: string
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Blog')
+    .setDescription('Blog description')
+    .setVersion('1.0')
+    .build();
+    
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(3000);
 }
