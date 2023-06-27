201ff1310827ae83cf80190949be4d40f3a04016
johannpa <johann_paque@hotmail.fr>
2023-06-27 15:02:35 +0200

    Add controller, service and Dto for comment

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,15 +1,35 @@
-import { Controller, Post, Put, Delete, Body, Param, Req } from '@nestjs/common';
+import { Controller, Post, Put, Delete, Body, Param, Req, UseGuards, ParseIntPipe } from '@nestjs/common';
 import { Request } from 'express';
 import { CommentService } from './comment.service';
 import { CreateCommentDto } from './dto/createCommentDto';
+import { AuthGuard } from '@nestjs/passport';
+import { UpdateCommentDto } from './dto/updateCommentDto';
 
 @Controller('comments')
 export class CommentController {
-    constructor(private readonly commentService : CommentService){}
-    
+    constructor(private readonly commentService: CommentService) { }
+
+    @UseGuards(AuthGuard('jwt'))
     @Post("create")
-    create(@Req() request : Request, @Body() createCommentDto: CreateCommentDto) {
+    create(@Req() request: Request, @Body() createCommentDto: CreateCommentDto) {
         const userId = request.user["userId"]
         return this.commentService.create(userId, createCommentDto)
     }
+
+    @UseGuards(AuthGuard("jwt"))
+    @Delete("delete/:id")
+    delete(@Req() request: Request, @Param("id", ParseIntPipe) commentId: number, @Body("postId") postId: number) {
+        const userId = request.user["userId"];
+        return this.commentService.delete(commentId, userId, postId)
+    }
+
+    @UseGuards(AuthGuard('jwt'))
+    @Put("update/:id")
+    update(@Param("id", ParseIntPipe) commentId: number, @Req() request: Request, @Body() updateCommentDto: UpdateCommentDto) {
+        const userId = request.user["userId"]
+        return this.commentService.update(commentId, userId, updateCommentDto)
+    }
 }
+
+
+
--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,13 +1,45 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { CreateCommentDto } from './dto/createCommentDto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { UpdateCommentDto } from './dto/updateCommentDto';
 
 @Injectable()
 export class CommentService {
+    
     constructor(private readonly prismaService: PrismaService){}
-    async create(userId: any, createCommentDto: CreateCommentDto) {
-        const {postId} = createCommentDto
+    async create(userId: number, createCommentDto: CreateCommentDto) {
+        const {postId, content} = createCommentDto
         const post = await this.prismaService.post.findUnique({ where : {postId}})
         if(!post) throw new NotFoundException("Post not found")
+        await this.prismaService.comment.create({
+            data : {
+                content, userId, postId
+            },
+        });
+        return {data: "comment Created!"};
+    }
+
+    async delete(commentId: number, userId: number, postId: number) {
+        const comment = await this.prismaService.comment.findFirst({where : {commentId}})
+        if(!comment) throw new NotFoundException('Comment not found');
+        if(comment.postId !== postId) throw new UnauthorizedException("Post id does not mathc")
+        if(comment.postId !== userId) throw new ForbiddenException("Forbiden action")
+        await this.prismaService.comment.delete({where : {commentId}})
+        return {data: "Comment deleted"}
+    }
+
+    async update(commentId: number, userId: any, updateCommentDto: UpdateCommentDto) {
+        const {content, postId} = updateCommentDto
+        const comment = await this.prismaService.comment.findFirst({
+            where: { commentId }
+        });
+        if(!comment) throw new NotFoundException('Comment not found');
+        if(comment.postId !== postId)
+            throw new UnauthorizedException('Post id does not match');
+        if(comment.userId !== userId)
+            throw new ForbiddenException("Forbiden action");
+
+            await this.prismaService.comment.update({where : {commentId}, data: {content}})
+            return {data: "comment Updated"}
     }
 }
