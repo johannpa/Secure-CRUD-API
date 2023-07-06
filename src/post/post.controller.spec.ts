e583f4d23b04e4551edbe52c50513be9968d37c3
johannpa <johann_paque@hotmail.fr>
2023-07-06 16:44:55 +0200

    Add jest and try to test some functionalities

--- src/post/post.controller.spec.ts
+++ src/post/post.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostController } from './post.controller';
+import { PostService } from './post.service';
 
 describe('PostController', () => {
   let controller: PostController;
@@ -7,12 +8,22 @@ describe('PostController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PostController],
+      providers: [PostService],
     }).compile();
 
     controller = module.get<PostController>(PostController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+  // it('should be defined', () => {
+  //   expect(controller).toBeDefined();
+  // });
+  describe('getAll', () => {
+    it('should return all posts', async () => {
+      const result = ['test'];
+
+      jest.spyOn(PostService, 'getAll').mockImplementation(() => result);
+      
+      expect(await PostController.getAll()).toBe(result);
+    })
+  })
 });
